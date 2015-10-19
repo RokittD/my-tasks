d8d581ed351763d341804d6524c2b9a48b351ed3
RokittD <sergey.tsykalovskyi@rokittech.com>
2015-10-19 16:02:51 +0300

    Create TasksController.php

---
+++ TasksController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace rokittd;
+use yii\web\Controller;
+
+class TasksController extends Controller
+{
+    public function actionTest(){
+        echo "ok, Rokitt Dynamix works";
+    }
+}
